7b382af517d43ca6d20d003a1f483b586ce35c20
VM <vm@ya.ru>
2022-06-26 12:20:14 -0400

    added print info

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Credit credit = new Credit(300000, 5.4, 60);
-        System.out.println(credit.calcOverpayment());
-        System.out.println(credit.calcMonthlyPayment());
-        System.out.println(credit.calcTotalCredit());
+        System.out.println("[Credit: $" + credit.getCreditTotal() + ", APR: " + credit.getCreditApr()+"%" + ", Months: " + credit.getCreditMonths()+"]");
+        System.out.println("Overpayment: $" + credit.calcOverpayment());
+        System.out.println("Monthly Payment: $" + credit.calcMonthlyPayment());
+        System.out.println("TotalC redit: $" + credit.calcTotalCredit());
 
     }
 }
